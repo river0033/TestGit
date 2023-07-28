32677cbb1a3b5140ee635b3a8efb32cd8507beef
River <river0033@outlook.com>
2023-07-28 12:26:20 +0800

    Initial Commit

---
+++ TestGit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestGit
+//
+//  Created by its_River on 2023/7/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by its_River on 2023/7/28.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
