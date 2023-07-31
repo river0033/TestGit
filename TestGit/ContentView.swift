a0ac4e3e53075aaa978188cd414a4ccc518970fa
River <river0033@outlook.com>
2023-07-31 12:41:17 +0800

    加了一个X

--- TestGit/ContentView.swift
+++ TestGit/ContentView.swift
@@ -30,6 +30,7 @@ struct ContentView: View {
             Text("Hello, world!")
             Text("Hello, world!")
             Text("Hello, world!")
+            Text("X")
         }
         .padding()
     }
